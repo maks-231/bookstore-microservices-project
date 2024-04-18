a843b1feb3c65f884df10bec9b7d743f21b8f640
mahtirschii <mahtirschii@amsoft-group.com>
2024-04-18 14:47:12 +0300

    bookstorage-service was added

---
+++ bookstore-service/src/main/java/store/book/BookStoreApplication.java
@@ -0,0 +1,19 @@
+package store.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+import lombok.extern.log4j.Log4j2;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@RefreshScope
+@Log4j2
+public class BookStoreApplication {
+  public static void main(String[] args) {
+    System.out.println("Hello");
+    SpringApplication.run(BookStoreApplication.class, args);
+  }
+}
---
+++ bookstore-service/src/main/java/store/book/BookStoreDiscoveryClient.java
@@ -0,0 +1,41 @@
+package store.book;
+
+import com.netflix.discovery.EurekaClient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import jakarta.annotation.Resource;
+
+@RestController
+@RequestMapping("/service-instances")
+public class BookStoreDiscoveryClient {
+  @Resource
+  private DiscoveryClient discoveryClient;
+
+  @Autowired
+  @Lazy
+  private EurekaClient eurekaClient;
+
+  @Value("${spring.application.name}")
+  private String appName;
+
+  @RequestMapping("/{applicationName}")
+  public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
+    return this.discoveryClient.getInstances(applicationName);
+  }
+
+  @RequestMapping("/greeting")
+  public String greeting() {
+    return String.format(
+        "Hello from '%s'!", eurekaClient.getApplication(appName).getName());
+  }
+}
