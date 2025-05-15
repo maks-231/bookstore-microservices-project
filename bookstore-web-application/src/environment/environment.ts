b4c86441d98914c1d8ea3c157c66fabce84fa87a
maxim <m.ahtirschii@gmai.com>
2025-05-15 14:57:10 +0300

    updating whole project

--- bookstore-web-application/src/environment/environment.ts
+++ bookstore-web-application/src/environment/environment.ts
@@ -1,6 +1,7 @@
 export const Environment = {
 //     URL: 'http://bookstore-service:8091/',
     URL: 'http://localhost:8091/',
+//     URL: 'http://bookstore-service:8091/',
 //     URL: 'http://localhost:8090/bookstore-backend/',
     BOOK: 'book',
     GENRE: 'genre',
